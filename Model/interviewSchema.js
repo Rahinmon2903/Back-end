3b83ec4c4f4906e75aff62dda3a8f3a49d852ce8
Rahinmon2903 <rahin2903@gmail.com>
2026-04-11 10:31:55 +0530

    interview Schema completed

---
+++ Model/interviewSchema.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const interviewSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        
+    },
+    role:{
+        type:String,
+        
+    },
+    difficulty:{
+        type:String,
+        enum:["easy","medium","hard"],
+        
+        
+    },
+    questions:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Question"
+    }],
+    createdBy:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
+    
+},{timestamps:true}
+);
+
+const Interview=mongoose.model("Interview",interviewSchema);
+export default Interview;
--- Router/userRoute.js
+++ Router/userRoute.js
@@ -4,6 +4,7 @@ import { protect, adminOnly } from "../Middleware/authMiddleware.js";
 const router = express.Router();
 
 //  Protected route
+//“In the protect middleware, we take user info and attach it to the request object. Then in the route handler, we send that data to the frontend.”
 router.get("/profile", protect, (req, res) => {
   res.status(200).json({
     message: "User profile fetched",
